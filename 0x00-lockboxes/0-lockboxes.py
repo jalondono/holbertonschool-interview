1111adad0f8984dc62d8726405c5d7200c556b77
jalondono <juanlondono151776@hotmail.com>
2020-04-21 18:26:00 -0500

    fixed issue with infinite loop

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,58 +4,16 @@
 
 def canUnlockAll(boxes):
     """You have n number of locked boxes in front of you."""
-    opened_boxes = {0: []}
+    id_opened_boxes = [0]
     number_boxes = len(boxes)
-    box_idx = 0
-    finish_open = 0
 
-    while finish_open == 0:
-        flag_go_back = True
-        if len(boxes[box_idx]) > 0:
-            for key in boxes[box_idx]:
-                if box_idx not in opened_boxes:
-                    opened_boxes[box_idx] = []
-                if key in opened_boxes[box_idx]:
-                    continue
-                """Recorrer los indices de las sublistas"""
-                if key > number_boxes:
-                    """si una key esta fuera de rango"""
-                    opened_boxes[box_idx].append(key)
-                    flag_go_back = False
-                    continue
-                if len(boxes[key]) == 0:
-                    """si una caja esta nula, marquela como abierta"""
-                    opened_boxes[key] = []
-                    opened_boxes[box_idx].append(key)
-                    flag_go_back = False
-                else:
-                    opened_boxes[box_idx].append(key)
-                    box_idx = key
-                    flag_go_back = False
-                    break
-        if len(opened_boxes) >= number_boxes:
-            return True
-        if flag_go_back:
-            count = 0
-            re_check = False
-            for key, value in opened_boxes.items():
-                if len(boxes[key]) != len(value):
-                    box_idx = key
-                    break
-                else:
-                    for element in value:
-                        if element < len(boxes):
-                            if len(boxes[element]) != len(opened_boxes[element]):
-                                """Revisa la lista del indice que la lista actual
-                                , sea igual a las que ya se han guardado en el diccionario"""
-                                box_idx = element
-                                re_check = True
-                                break
-                            else:
-                                continue
-                    if re_check:
-                        break
-                count += 1
-                if count == len(opened_boxes) - 1:
-                    """Si termina de revisar todas las cajas y falntan"""
-                    return False
+    for id_open in id_opened_boxes:
+        for current_id in boxes[id_open]:
+            if current_id < number_boxes and current_id not in id_opened_boxes:
+                id_opened_boxes.append(current_id)
+    if len(id_opened_boxes) == number_boxes:
+        return True
+    return False
+
+
+
