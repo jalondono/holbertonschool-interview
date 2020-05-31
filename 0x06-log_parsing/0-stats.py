feb07d5579011dec8698f1d7dadc3b60d51a7016
jalondono <juanlondono151776@hotmail.com>
2020-05-31 17:23:15 -0500

    upload task 0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,15 +6,16 @@ import signal
 
 def main():
     count = 0
-    data = {}
+    data = {'200': 0, '301': 0, '400': 0, '401': 0,
+            '403': 0, '404': 0, '405': 0, '500': 0}
     size = 0
 
     def signal_handler(*args):
         print("File size: {}".format(size))
-        for key, value in data.items():
+        for key in sorted(data.keys()):
             if data[key] != 0:
-                print("{}: {}".format(key, value))
-        sys.exit()
+                print("{}: {}".format(key, data[key]))
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
 
@@ -23,16 +24,17 @@ def main():
         status = response[0]
         size += int(response[1][:-1])
 
-        if status not in data:
-            data[status] = 1
-        else:
-            data[status] += 1
+        # if status not in data:
+        #     data[status] = 1
+        # else:
+        #     data[status] += 1
+        data[status] += 1
 
         if count >= 9:
             print("File size: {}".format(size))
-            for key, value in data.items():
+            for key in sorted(data.keys()):
                 if data[key] != 0:
-                    print("{}: {}".format(key, value))
+                    print("{}: {}".format(key, data[key]))
             count = 0
         count += 1
 
