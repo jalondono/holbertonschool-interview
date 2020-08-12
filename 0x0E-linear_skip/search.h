383a8a9b21a8ac566e0b887173de74e8ed5e69e3
jalondono <juanlondono151776@hotmail.com>
2020-08-12 18:57:40 -0500

    upload taskl 0

---
+++ 0x0E-linear_skip/search.h
@@ -0,0 +1,27 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+skiplist_t *linear_skip(skiplist_t *list, int value);
+void free_skiplist(skiplist_t *list);
+void init_express(skiplist_t *list, size_t size);
+skiplist_t *create_skiplist(int *array, size_t size);
+
+#endif
