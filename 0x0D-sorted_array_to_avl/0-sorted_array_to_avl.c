00217f5bfaef56e145ccd72f1f416ed2d0403b81
jalondono <juanlondono151776@hotmail.com>
2020-08-12 18:50:06 -0500

    betty

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -10,42 +10,42 @@
  **/
 avl_t *bst_insert(avl_t **tree, int value)
 {
-    binary_tree_t *root;
+	binary_tree_t *root;
 
-    if (!tree)
-        return (NULL);
-    root = *tree;
-    if (!root)
-    {
-        *tree = binary_tree_node(root, value);
-        return (*tree);
-    }
-    while (root->left || root->right)
-    {
-        if (root && (root->n > value))
-        {
-            if (root->left)
-                root = root->left;
-            else
-                break;
-        }
-        else if (root && (root->n < value))
-        {
-            if (root->right)
-                root = root->right;
-            else
-                break;
-        }
-        else if (root && (root->n == value))
-            return (NULL);
-    }
-    if (root && (root->n == value))
-        return (NULL);
-    if (root && (root->n > value))
-        return (insert_left(root, value));
-    else if (root && (root->n < value))
-        return (insert_right(root, value));
-    return (NULL);
+	if (!tree)
+		return (NULL);
+	root = *tree;
+	if (!root)
+	{
+		*tree = binary_tree_node(root, value);
+		return (*tree);
+	}
+	while (root->left || root->right)
+	{
+		if (root && (root->n > value))
+		{
+			if (root->left)
+				root = root->left;
+			else
+				break;
+		}
+		else if (root && (root->n < value))
+		{
+			if (root->right)
+				root = root->right;
+			else
+				break;
+		}
+		else if (root && (root->n == value))
+			return (NULL);
+	}
+	if (root && (root->n == value))
+		return (NULL);
+	if (root && (root->n > value))
+		return (insert_left(root, value));
+	else if (root && (root->n < value))
+		return (insert_right(root, value));
+	return (NULL);
 }
 /**
  * array_to_avl - recursively set an AVL tree
@@ -57,12 +57,12 @@ avl_t *bst_insert(avl_t **tree, int value)
  */
 avl_t **array_to_avl(int *array, size_t size, avl_t **root)
 {
-    if (size < 1)
-        return (NULL);
-    bst_insert(root, array[(size - 1) / 2]);
-    array_to_avl(array + (size + 1) / 2, size / 2, root);
-    array_to_avl(array, size / 2, root);
-    return (root);
+	if (size < 1)
+		return (NULL);
+	bst_insert(root, array[(size - 1) / 2]);
+	array_to_avl(array + (size + 1) / 2, size / 2, root);
+	array_to_avl(array, size / 2, root);
+	return (root);
 
 }
 /**
@@ -74,13 +74,13 @@ avl_t **array_to_avl(int *array, size_t size, avl_t **root)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    avl_t *root;
+	avl_t *root;
 
-    if (!array)
-        return (NULL);
-    root = NULL;
-    bst_insert(&root, array[(size - 1) / 2]);
-    array_to_avl(array + (size + 1) / 2, size / 2, &root);
-    array_to_avl(array, size / 2, &root);
-    return (root);
+	if (!array)
+		return (NULL);
+	root = NULL;
+	bst_insert(&root, array[(size - 1) / 2]);
+	array_to_avl(array + (size + 1) / 2, size / 2, &root);
+	array_to_avl(array, size / 2, &root);
+	return (root);
 }
